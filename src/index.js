6da86214797d5073e7338db55bfecacc1aa11fce
MukarramHaq <mukarram796@gmail.com>
2024-12-30 18:04:01 -0500

    feat: completed turn switching and changing the color of the takes the round paragraph with the color of the winner

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const oBtnElement = document.querySelector('.o-btn');
 const xSvgElement = document.querySelector('.x-svg');
 const oSvgElement = document.querySelector('.o-svg');
 
+// Turn Button
+const turnSvgEl = document.querySelector('.turn-svg');
+
 // Lightbox elements
 const lightboxEl = document.querySelector('.lightbox');
 const whoWonEl = document.querySelector('.who-won');
@@ -55,9 +58,15 @@ restartBtnEl.addEventListener('click', () => {
         }else{
             element.innerHTML = `<div class="o-element hover-properties">
             <svg width="66" height="66" xmlns="http://www.w3.org/2000/svg"><path d="M33 1c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C15.327 65 1 50.673 1 33 1 15.327 15.327 1 33 1Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" stroke="#F2B137" stroke-width="2" fill="none"/></svg>
-          </div>`
+            </div>`
+            turnSvgEl.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#A8BFC9" fill-rule="evenodd"/></svg>`  
         }
-    })
+    });
+
+    if(oSelected){
+        setTimeout(() => cpuMove(board), 1000);
+    }
+
 })
 
 // Mapping the game board elements to the logical board
@@ -123,10 +132,9 @@ cpuBtnElement.addEventListener('click', () => {
                 // To disable player interaction when its the CPU's turn
                 if(!isPlayerTurn) return;
                 if(gameEnded) return;
-    
                 const row = element.dataset.row;
                 const column = element.dataset.column;
-                
+                turnSvgEl.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#A8BFC9"/></svg>`
                 playerMove(element, row, column);
             })
         })
@@ -142,90 +150,36 @@ cpuBtnElement.addEventListener('click', () => {
         if(!isPlayerTurn){
             return;
         }else{
+            turnSvgEl.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#A8BFC9"/></svg>`
             gameBoardElement.forEach(element => {
                 element.addEventListener('click', () => {
                     const row = element.dataset.row;
                     const column = element.dataset.column;
                     playerMove(element, row, column);
-                    // console.log(board);
+                    turnSvgEl.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#A8BFC9" fill-rule="evenodd"/></svg>`
                 })
             })
         }
-        // console.log(board);
     }
     
 });
 
 //To check for a winner
-
 const checkWinner = (board) => {
-
-
     for(let i = 0; i < board.length; i++){
-
         // Check rows
         if(checkWinningLine('X', [[i, 0], [i, 1], [i, 2]])) return;
-        
         if(checkWinningLine('O', [[i, 0], [i, 1], [i, 2]])) return;;
-
         // Check columns
         if (checkWinningLine('X', [[0, i], [1, i], [2, i]])) return;
         if (checkWinningLine('O', [[0, i], [1, i], [2, i]])) return;
-
     }
-
     // Check diagonals
     if (checkWinningLine('X', [[0, 0], [1, 1], [2, 2]])) return;
     if (checkWinningLine('O', [[0, 0], [1, 1], [2, 2]])) return
 
     if (checkWinningLine('X', [[0, 2], [1, 1], [2, 0]])) return;
     if (checkWinningLine('O', [[0, 2], [1, 1], [2, 0]])) return;
-
-    // // For diagonal
-    // if(checkWinningLine('X', [board[0][0], board[1][1], board[2][2]])) return;
-    // if(checkWinningLine('O', [board[0][0], board[1][1], board[2][2]])) return;
-
-    // if(checkWinningLine('X', [board[0][2], board[1][1], board[2][0]])) return;
-    // if(checkWinningLine('O', [board[0][2], board[1][1], board[2][0]])) return;
-
-
-    // For row
-    // for(let i = 0; i < board.length; i++){
-    //     if(board[i][0] === 'X' && board[i][1] === 'X' && board[i][2] === 'X'){
-    //         alert('The winner is X');
-    //         return gameEnded = true;
-    //         // return;
-    //     }
-    //     if(board[i][0] === 'O' && board[i][1] === 'O' && board[i][2] === 'O'){
-    //         alert('The winner is O');
-    //         return gameEnded = true;
-    //     }
-    // }
-
-    // // For column
-    // for(let i = 0; i < board.length; i++){
-    //     if(board[0][i] === 'X' && board[1][i] === 'X' && board[2][i] === 'X'){
-    //         alert('The winner is X');
-    //         return gameEnded = true;
-    //     }
-    //     if(board[0][i] === 'O' && board[1][i] === 'O' && board[2][i] === 'O'){
-    //         alert('The winner is O');
-    //         return gameEnded = true;
-    //     }
-    // }
-
-    // // For diagonal
-    
-    // if((board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') || (board[0][2] === 'X' && board[1][1] === 'X' && board[2][2] === 'X')){
-    //     alert('The winner is X');
-    //     return gameEnded = true;
-    // }
-    // if((board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O') || (board[0][2] === 'O' && board[1][1] === 'O' && board[2][2] === 'O')){
-    //     alert('The winner is O')
-    //     return gameEnded = true;
-    // }
-    
-
     // For draw
     let flattenedBoard = board.flat();
     if(!flattenedBoard.includes(null)){
@@ -241,7 +195,6 @@ const playerMove = (element, row, column) => {
         if(oSelected){
             board[row][column] = 'O'; // The logical board is updated. This is my signal to switch the turn to the CPU
             element.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>`
-            // console.log('The position that is clicked is:', [row, column]);
         }else{
             board[row][column] = 'X';
             element.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>`
@@ -276,8 +229,10 @@ const cpuMove = (board) => {
         );
         if(oSelected){
             cpuElement.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>`
+            turnSvgEl.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#A8BFC9"/></svg>`
         }else{
             cpuElement.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>`
+            turnSvgEl.innerHTML = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#A8BFC9" fill-rule="evenodd"/></svg>`
         }
         if(checkWinner(board)) return;  
         isPlayerTurn = true;
@@ -318,7 +273,7 @@ const lightboxDisplay = (winner) => {
     if (winner === 'X') {
         xWonEl.classList.remove('hidden');
         oWonEl.classList.add('hidden');
-        
+        takesRoundEl.style.color = '#31C3BD';
         // If the player selected X, they won; otherwise, they lost
         whoWonEl.textContent = xSelected ? 'You won!' : 'Oh no, you lost...';
     } else if (winner === 'O') {
@@ -335,5 +290,5 @@ const lightboxDisplay = (winner) => {
     }
 
     // Common message for all cases
-    takesRoundEl.textContent = 'takes the round';
+    // takesRoundEl.textContent = 'takes the round';
 };
